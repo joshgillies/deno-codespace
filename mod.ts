b9da65012e572e54e0bb99283a90cede32262948
Josh Gillies <hey+github@josh.gl>
2020-10-28 13:48:41 +0000

    Update project with new settings and demo program

--- mod.ts
+++ mod.ts
@@ -1,14 +1,16 @@
-import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
-const server = serve({ port: 8000 });
+import { serve } from "https://deno.land/std@0.75.0/http/server.ts";
+import { getCodespaceURL } from "https://deno.land/x/codespace_url/mod.ts";
+const port = 8000;
+const server = serve({ port });
 
-const headers = new Headers();
-headers.set("content-type", "text/plain");
-
-console.log("http://localhost:8000/")
+console.log(getCodespaceURL(port));
 for await (const req of server) {
-    req.respond({
-        headers,
-        status: 200,
-        body: "Hello World\n"
-    });
+  const headers = new Headers();
+  headers.set("content-type", "text/html;charset=utf8");
+
+  req.respond({
+    headers,
+    status: 200,
+    body: `Welcome to Deno ${Deno.version.deno} 🦕`,
+  });
 }
